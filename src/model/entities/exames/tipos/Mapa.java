951efa79823a5b2423be60e8d91f32e2383a0416
Michelly Nonato <smartnovo01@gmail.com>
2024-04-25 17:45:43 -0300

    [Create] Class Mapa

---
+++ src/model/entities/exames/tipos/Mapa.java
@@ -0,0 +1,11 @@
+package model.entities.exames.tipos;
+
+import model.entities.BaseEntity;
+import model.entities.exames.Exame;
+
+public class Ecocardiograma extends BaseEntity {
+	String mediaHoras;
+	String paVirgula;
+	String paSono;
+	Exame exame;
+}
