ffcd94cce7b1bd6dd4a823f63a2a468b5d199a84
Caique Piazzaroli <caique405@gmail.com>
2024-04-25 16:19:43 -0300

    new class: convenio, exame, histMedico, horarioExame, paciente

---
+++ src/model/entities/Exame.java
@@ -0,0 +1,11 @@
+package model.entities;
+
+public class Exame extends BaseEntity {
+
+	Double peso;
+	String funcionarioResponsavel;
+	String conclusoes;
+	String detalhes;
+	int diagnosticoClinico;
+	
+}
---
+++ src/model/entities/Paciente.java
@@ -0,0 +1,6 @@
+package model.entities;
+
+public class Paciente extends BaseEntity {
+
+	
+}
