24379418bfff0428bd095fe4783778cbae551ce0
paulaandrezza <paulaandrezza25@gmail.com>
2024-04-30 18:03:24 -0300

    fix: fix enums

--- resources/sql/create_tables.sql
+++ resources/sql/create_tables.sql
@@ -1,3 +1,15 @@
+-- table Endereco
+CREATE TABLE Endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cep INT NOT NULL,
+    estado VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    numero VARCHAR(10),
+    complemento VARCHAR(255)
+);
+
 -- table Pessoa
 CREATE TABLE IF NOT EXISTS Pessoa (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,5 +18,36 @@ CREATE TABLE IF NOT EXISTS Pessoa (
     dataNascimento DATE NOT NULL,
     celular BIGINT,
     email VARCHAR(255) UNIQUE,
-    genero INTEGER NOT NULL
+    genero INTEGER NOT NULL,
+    endereco_id INT,
+    FOREIGN KEY (endereco_id) REFERENCES Endereco(id)
 );
+
+-- table Convenio
+CREATE TABLE Convenio (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numeroCarteirinha INT NOT NULL,
+    prestadora VARCHAR(255) NOT NULL,
+    plano VARCHAR(255) NOT NULL
+);
+
+-- table HistoricoMedico
+CREATE TABLE HistoricoMedico (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    alergias TEXT NOT NULL,
+    medicamentos TEXT NOT NULL,
+    prescricao TEXT NOT NULL
+);
+
+-- table Paciente
+CREATE TABLE Paciente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    altura FLOAT NOT NULL,
+    fumante BOOLEAN NOT NULL,
+    marcaPasso BOOLEAN NOT NULL,
+    convenio_id INT,
+    historicoMedico_id INT,
+    FOREIGN KEY (convenio_id) REFERENCES Convenio(id),
+    FOREIGN KEY (historicoMedico_id) REFERENCES HistoricoMedico(id)
+);
+
--- src/model/enums/EspecialidadeMedica.java
+++ src/model/enums/EspecialidadeMedica.java
@@ -1,14 +1,24 @@
 package model.enums;
 
 public enum EspecialidadeMedica {
-	CARDIOLOGIA,
-    DERMATOLOGIA,
-    ENDOCRINOLOGIA,
-    GASTROENTEROLOGIA,
-    GINECOLOGIA,
-    NEUROLOGIA,
-    ORTOPEDIA,
-    PEDIATRIA,
-    PSIQUIATRIA,
-    UROLOGIA;
+    CARDIOLOGIA(1),
+    DERMATOLOGIA(2),
+    ENDOCRINOLOGIA(3),
+    GASTROENTEROLOGIA(4),
+    GINECOLOGIA(5),
+    NEUROLOGIA(6),
+    ORTOPEDIA(7),
+    PEDIATRIA(8),
+    PSIQUIATRIA(9),
+    UROLOGIA(10);
+
+    private final int value;
+
+    private EspecialidadeMedica(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
