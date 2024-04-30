630d7d69bd09690ae8b0358e61c67f167b7d16cf
paulaandrezza <paulaandrezza25@gmail.com>
2024-04-30 18:20:52 -0300

    feat: add some tables

--- resources/sql/create_tables.sql
+++ resources/sql/create_tables.sql
@@ -12,13 +12,13 @@ CREATE TABLE Endereco (
 
 -- table Pessoa
 CREATE TABLE IF NOT EXISTS Pessoa (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(255) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     dataNascimento DATE NOT NULL,
     celular BIGINT,
     email VARCHAR(255) UNIQUE,
-    genero INTEGER NOT NULL,
+    genero INT NOT NULL,
     endereco_id INT,
     FOREIGN KEY (endereco_id) REFERENCES Endereco(id)
 );
@@ -34,9 +34,9 @@ CREATE TABLE Convenio (
 -- table HistoricoMedico
 CREATE TABLE HistoricoMedico (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    alergias TEXT NOT NULL,
-    medicamentos TEXT NOT NULL,
-    prescricao TEXT NOT NULL
+    alergias TEXT,
+    medicamentos TEXT,
+    prescricao TEXT
 );
 
 -- table Paciente
@@ -50,4 +50,30 @@ CREATE TABLE Paciente (
     FOREIGN KEY (convenio_id) REFERENCES Convenio(id),
     FOREIGN KEY (historicoMedico_id) REFERENCES HistoricoMedico(id)
 );
+
+-- table Funcionario
+CREATE TABLE Funcionario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    emailCorporativo VARCHAR(50) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+);
+
+-- table Medico
+CREATE TABLE Medico (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    funcionario_id INT,
+    especialidade INT NOT NULL,
+    FOREIGN KEY (funcionario_id) REFERENCES Funcionario(id)
+);
+
+-- table HorarioAtendimento
+CREATE TABLE HorarioAtendimento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    medico_id INT,
+    diaDaSemana INT NOT NULL,
+    horarioInicio TIME NOT NULL,
+    horarioFim TIME NOT NULL,
+    FOREIGN KEY (medico_id) REFERENCES Medico(id)
+);
+
 
