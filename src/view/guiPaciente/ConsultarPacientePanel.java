2d8b51504a5c3b60ea5a2c7fa860691d4cec0ad3
Michelly Nonato <smartnovo01@gmail.com>
2024-06-20 16:55:00 -0300

    feat: message delete sucess in paciente

--- src/view/guiPaciente/ConsultarPacientePanel.java
+++ src/view/guiPaciente/ConsultarPacientePanel.java
@@ -1,5 +1,6 @@
 package view.guiPaciente;
 
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,7 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Box;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,6 +20,9 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 import controller.PacienteController;
 import model.entities.Convenio;
@@ -60,13 +66,22 @@ public class ConsultarPacientePanel extends JInternalFrame {
 		textPesquisar.setColumns(10);
 		boxVerticalListaPaciente.add(textPesquisar);
 
-		table = new JTable();
-		table.setEnabled(false);
+		table = new JTable() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return column == 4 || column == 5;
+			}
+		};
+		table.setShowVerticalLines(false);
+		table.setFont(new Font("Verdana", Font.PLAIN, 10));
+		table.setRowSelectionAllowed(false);
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(10, 120, 750, 650);
 		getContentPane().add(scrollPane);
 
-		String[] columnNames = { "Nome do Paciente", "RG", "CPF", "Plano" };
+		String[] columnNames = { "Nome do Paciente", "RG", "CPF", "Plano", "Editar", "Deletar", "ID" };
 		tableModel = new DefaultTableModel(columnNames, 0);
 		table.setModel(tableModel);
 
@@ -84,6 +99,24 @@ public class ConsultarPacientePanel extends JInternalFrame {
 				}
 			}
 		});
+
+		TableColumnModel columnModel = table.getColumnModel();
+		int[] columnWidths = { 160, 80, 80, 150, 70, 70, 0 };
+		for (int i = 0; i < columnWidths.length; i++) {
+			TableColumn column = columnModel.getColumn(i);
+			column.setPreferredWidth(columnWidths[i]);
+			if (i == 6) {
+				column.setMinWidth(0);
+				column.setMaxWidth(0);
+				column.setWidth(0);
+				column.setPreferredWidth(0);
+			}
+		}
+
+		columnModel.getColumn(4).setCellRenderer(new ButtonRenderer());
+		columnModel.getColumn(4).setCellEditor(new ButtonEditor(new JButton("Editar")));
+		columnModel.getColumn(5).setCellRenderer(new ButtonRenderer());
+		columnModel.getColumn(5).setCellEditor(new ButtonEditor(new JButton("Deletar")));
 	}
 
 	private void loadAllPatients() {
@@ -99,7 +132,7 @@ public class ConsultarPacientePanel extends JInternalFrame {
 			Convenio convenio = paciente.getConvenio();
 			String plano = convenio.getPlano();
 
-			Object[] rowData = { nome, rg, cpf, plano };
+			Object[] rowData = { nome, rg, cpf, plano, "Editar", "Deletar", paciente.getId() };
 			tableModel.addRow(rowData);
 		}
 	}
@@ -126,9 +159,89 @@ public class ConsultarPacientePanel extends JInternalFrame {
 			Convenio convenio = paciente.getConvenio();
 			String plano = convenio.getPlano();
 
-			Object[] rowData = { nome, rg, cpf, plano };
+			Object[] rowData = { nome, rg, cpf, plano, "Editar", "Deletar", paciente.getId() };
 			tableModel.addRow(rowData);
 		}
 	}
 
+	@SuppressWarnings("serial")
+	class ButtonRenderer extends JButton implements TableCellRenderer {
+		public ButtonRenderer() {
+			setOpaque(true);
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			setText((value == null) ? "" : value.toString());
+			return this;
+		}
+	}
+
+	@SuppressWarnings("serial")
+	class ButtonEditor extends DefaultCellEditor {
+		private String label;
+		private JButton button;
+		private boolean isPushed;
+
+		public ButtonEditor(JButton button) {
+			super(new JCheckBox());
+			this.button = button;
+			this.button.setOpaque(true);
+			this.button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					fireEditingStopped();
+				}
+			});
+		}
+
+		@Override
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+				int column) {
+			label = (value == null) ? "" : value.toString();
+			button.setText(label);
+			isPushed = true;
+			return button;
+		}
+
+		@Override
+		public Object getCellEditorValue() {
+			if (isPushed) {
+				int selectedRow = table.getSelectedRow();
+				int patientId = (Integer) table.getValueAt(selectedRow, 6); // ID is in the 6th column
+				if (label.equals("Editar")) {
+					System.out.println("Botão Editar pressionado na linha: " + selectedRow + " com ID: " + patientId);
+					// Adicione a lógica para editar o paciente aqui
+				} else if (label.equals("Deletar")) {
+					System.out.println("Botão Deletar pressionado na linha: " + selectedRow + " com ID: " + patientId);
+
+					String menssagem = String.format(
+							"Essa ação irá deletar os dados de %s%nClique em OK se deseja continuar com a ação.",
+							table.getValueAt(selectedRow, 0));
+
+					int resposta = JOptionPane.showConfirmDialog(null, menssagem, "Warning",
+							JOptionPane.OK_CANCEL_OPTION);
+					if (resposta == JOptionPane.OK_OPTION) {
+						DeletarPaciente delete = new DeletarPaciente(patientId);
+						JOptionPane.showMessageDialog(null, "Paciente deletado com sucesso!", "Sucesso",
+								JOptionPane.INFORMATION_MESSAGE);
+					}
+
+				}
+			}
+			isPushed = false;
+			return new String(label);
+		}
+
+		@Override
+		public boolean stopCellEditing() {
+			isPushed = false;
+			return super.stopCellEditing();
+		}
+
+		@Override
+		protected void fireEditingStopped() {
+			super.fireEditingStopped();
+		}
+	}
 }
---
+++ src/view/guiPaciente/DeletarPaciente.java
@@ -0,0 +1,15 @@
+package view.guiPaciente;
+
+public class DeletarPaciente {
+	private int id;
+
+	public DeletarPaciente(int id) {
+		super();
+		this.id = id;
+	}
+
+	public void delete() {
+
+	}
+
+}
